faf057a6b17cb5e1a407deed1774c6df351f951e
samoorai <hirotakatech@gmail.com>
2022-07-10 23:23:33 +0200

    Initial structure

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
 
+func main() {
+	tea.NewProgram(&Model{}).Start()
+}
+
+type Model struct{}
+
+func (m *Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m *Model) View() string {
+	return ""
 }
