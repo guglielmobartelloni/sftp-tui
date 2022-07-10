f57c7679bb06afb3520e1f2aef4767123cea3fe6
samoorai <hirotakatech@gmail.com>
2022-07-10 23:54:26 +0200

    Added q to quit

--- main.go
+++ main.go
@@ -26,12 +26,12 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c":
+		case "ctrl+c", "q":
 			return m, tea.Quit
 		}
-
 	}
 	return m, nil
+
 }
 
 func (m *Model) View() string {
