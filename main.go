35b5f5edf75c880ceab899e193de38d1ce9f61db
samoorai <hirotakatech@gmail.com>
2022-07-10 23:24:40 +0200

    Added error handling

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	tea.NewProgram(&Model{}).Start()
+	err := tea.NewProgram(&Model{}).Start()
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
 type Model struct{}
