4093a90ffef164828f01c6651a53402bfb01afb5
samoorai <hirotakatech@gmail.com>
2022-07-28 02:19:41 +0200

    Added item struct

--- main.go
+++ main.go
@@ -1,36 +1,5 @@
 package main
 
-import (
-	"github.com/charmbracelet/bubbles/list"
-	"github.com/charmbracelet/lipgloss"
-)
-
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
-
 func main() {
-
-}
-
-func createItemList() []list.Item {
-
-	sshClient := ConnectSSH("samoorai", "/Users/samurai/.ssh/id_rsa", "", "midas.usbx.me", "22", "/Users/samurai/.ssh/known_hosts")
-
-	walker := &walker{
-		sshClient:  sshClient,
-		currentDir: "./",
-	}
-
-	// walker.GetFile("banana", "/Users/samurai/Documents/progetti/ftp-tui/test")
-
-	items := []list.Item{}
-	fileList, err := walker.Ls()
-	handleError(err)
-
-	for _, value := range fileList {
-		item := &item{title: value}
-		items = append(items, item)
-	}
-
-	return items
 
 }
--- tui.go
+++ tui.go
@@ -1,1 +1,36 @@
 package main
+
+import "github.com/charmbracelet/bubbles/list"
+
+type item struct {
+	title       string
+	description string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.description }
+func (i item) FilterValue() string { return i.title }
+
+func createItemList() []list.Item {
+
+	sshClient := ConnectSSH("samoorai", "/Users/samurai/.ssh/id_rsa", "", "midas.usbx.me", "22", "/Users/samurai/.ssh/known_hosts")
+
+	walker := &walker{
+		sshClient:  sshClient,
+		currentDir: "./",
+	}
+
+	// walker.GetFile("banana", "/Users/samurai/Documents/progetti/ftp-tui/test")
+
+	items := []list.Item{}
+	fileList, err := walker.Ls()
+	handleError(err)
+
+	for _, value := range fileList {
+		item := &item{title: value}
+		items = append(items, item)
+	}
+
+	return items
+
+}
